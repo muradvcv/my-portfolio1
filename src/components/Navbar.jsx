e100da688007f09df93021c97b44ddb6ae71fdcf
muradvcv <muradvcv@gmail.com>
2026-05-17 00:41:55 +0600

    pdf problem slove

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -141,12 +141,12 @@ const Navbar = () => {
 
           {/* Resume */}
           <a
-            href="/resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-1 text-center px-5 py-3 rounded-2xl border border-purple-500/40 text-purple-400 hover:bg-purple-500/20 hover:border-purple-400 transition-all duration-300"
+            href="/MD_KHALEKUZZAMAN_Resume.pdf"
+            download="MD_KHALEKUZZAMAN_Resume.pdf"
+            className="inline-flex items-center justify-center px-6 py-2 rounded-2xl border border-purple-500/40 text-purple-400 hover:bg-purple-500/10 hover:scale-105 transition-all duration-300"
           >
             Resume
+            <MdDownload className="text-xl ml-2 text-purple-400" />
           </a>
 
         </div>
